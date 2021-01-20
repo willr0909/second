019be44d444a6dd55ed16c252d3a5f09de26b567
p.m.student <wramirez@students.tulsatech.edu>
2021-01-20 14:37:08 -0600

    Initial Commit

---
+++ second/secondApp.swift
@@ -0,0 +1,17 @@
+//
+//  secondApp.swift
+//  second
+//
+//  Created by P.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct secondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
